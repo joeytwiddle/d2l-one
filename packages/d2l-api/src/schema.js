00690580cf433305942ab3ef784ece3e0cca58c5
Paul Joey Clark <joeytwiddle@gmail.com>
2021-12-01 05:28:09 +0800

    Store schema in a separate file

--- packages/d2l-api/src/schema.js
+++ packages/d2l-api/src/schema.js
@@ -1,10 +1,7 @@
+var fs = require('fs');
 var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
+var schema = buildSchema(fs.readFileSync('./schema.gql', 'utf8'));
 
 module.exports = schema;
