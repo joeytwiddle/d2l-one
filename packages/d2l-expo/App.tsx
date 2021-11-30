7578a817e2747741bd65c3c5e33c0ca45b0a3323
Paul Joey Clark <joeytwiddle@gmail.com>
2021-12-01 07:26:18 +0800

    Set up ApolloProvider

--- packages/d2l-expo/App.tsx
+++ packages/d2l-expo/App.tsx
@@ -1,10 +1,16 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+// TODO: Change url depending on deployment
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,10 +20,12 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <ApolloProvider client={client}>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      </ApolloProvider>
     );
   }
 }
