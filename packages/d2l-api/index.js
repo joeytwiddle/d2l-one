c719f4d23d33efd4aa2d32f9ca3b6e19b7ddb766
Paul Joey Clark <joeytwiddle@gmail.com>
2021-12-01 08:14:26 +0800

    Log incoming HTTP requests

--- packages/d2l-api/index.js
+++ packages/d2l-api/index.js
@@ -8,8 +8,29 @@ var app = express();
 
 app.use(cors());
 
+// This is only needed so that we can see the value of body in our logging function
+app.use(express.json());
+
+const logRequestMiddleware = async (req, res, next) => {
+  // Not sure this code is safe, better wrap it in try-catch!
+  try {
+    const { ip, method, url, query, params, body } = req;
+    const queryString = Object.keys(query).length > 0 ? '?' + JSON.stringify(query) : '';
+    const paramsString = Object.keys(params).length > 0 ? JSON.stringify(params) : '';
+    // We make a copy of body so that we can cleanup the query string
+    const bodyData = JSON.parse(JSON.stringify(body) || 'null');
+    if (bodyData?.query) bodyData.query = bodyData.query.replace(/[ \n\t]+/g, ' ');
+    const bodyString = bodyData ? JSON.stringify(bodyData) : '';
+    console.log(`${new Date().toISOString()} [${ip}] ${method} ${url}${queryString} ${paramsString || bodyString}`);
+  } catch (error) {
+    console.error(error);
+  }
+  next();
+};
+
 app.use(
   '/graphql',
+  logRequestMiddleware,
   graphqlHTTP({
     schema: schema,
     rootValue: root,
