5eebcbab03b2679bbcd0cafc4ca9769236593684
Paul Joey Clark <joeytwiddle@gmail.com>
2021-12-07 22:39:06 +0800

    Try to render a scrolling table (this scrolls on mobile but not on web)

--- packages/d2l-expo/screens/RescuesScreen.tsx
+++ packages/d2l-expo/screens/RescuesScreen.tsx
@@ -1,15 +1,80 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { ScrollView, StyleSheet } from 'react-native';
+import { DataTable } from 'react-native-paper';
+import { View } from '../components/Themed';
 
 export default function RescuesScreen() {
   return (
     <View style={styles.container}>
+      {/*
       <Text style={styles.title}>Rescues</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/RescuesScreen.tsx" />
+      */}
+      <ScrollView horizontal>
+        <DataTable>
+          <DataTable.Header>
+            <DataTable.Title style={styles.cell}>Date</DataTable.Title>
+            <DataTable.Title style={styles.cell}>TM</DataTable.Title>
+            <DataTable.Title style={styles.cell}>PS</DataTable.Title>
+            <DataTable.Title style={styles.cell}>TM</DataTable.Title>
+            <DataTable.Title style={styles.cell}>PS</DataTable.Title>
+            <DataTable.Title style={styles.cell}>TM</DataTable.Title>
+            <DataTable.Title style={styles.cell}>PS</DataTable.Title>
+            <DataTable.Title style={styles.cell}>TM</DataTable.Title>
+            <DataTable.Title style={styles.cell}>PS</DataTable.Title>
+            <DataTable.Title style={styles.cell}>TM</DataTable.Title>
+            <DataTable.Title style={styles.cell}>PS</DataTable.Title>
+            <DataTable.Title style={styles.cell}>TM</DataTable.Title>
+            <DataTable.Title style={styles.cell}>PS</DataTable.Title>
+          </DataTable.Header>
+          <DataTable.Row>
+            <DataTable.Cell style={styles.cell}>Mon 1 Dec</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell style={styles.cell}>Tue 2 Dec</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.Cell style={styles.cell}>Wed 3 Dec</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Jennifer</DataTable.Cell>
+            <DataTable.Cell style={styles.cell}>Max</DataTable.Cell>
+          </DataTable.Row>
+        </DataTable>
+      </ScrollView>
+      {/* <EditScreenInfo path="/screens/RescuesScreen.tsx" /> */}
     </View>
   );
 }
@@ -28,5 +93,9 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  cell: {
+    width: 80,
+    padding: 6,
   },
 });
