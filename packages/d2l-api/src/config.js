4544a3e71b561aab20209749e61eb15b0e93cb4e
Paul Joey Clark <joeytwiddle@gmail.com>
2021-12-11 05:54:47 +0800

    Implement reading of local config file

--- packages/d2l-api/src/config.js
+++ packages/d2l-api/src/config.js
@@ -16,7 +16,18 @@ const configsByEnv = {
 
 const configForEnv = configsByEnv[nodeEnv];
 
-const localConfig = {}; // TODO: Read this from local file
+let localConfig = {}; // TODO: Read this from local file
+const localConfigFile = `${require('os').homedir()}/.config/d2l-api-config.js`;
+try {
+  localConfig = require(localConfigFile);
+  console.log(`Adding local config from ${localConfigFile}`);
+} catch (error) {
+  if (error.code === 'MODULE_NOT_FOUND') {
+    console.log(`Not adding local config from ${localConfigFile}`);
+  } else {
+    console.warn(`Failed to read local config file ${localConfigFile}:`, error);
+  }
+}
 
 const config = {
   ...defaultConfig,
