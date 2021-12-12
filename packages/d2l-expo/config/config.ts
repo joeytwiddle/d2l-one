bfd2335aa13ed3d66c4d895c82333001431ff281
Paul Joey Clark <joeytwiddle@gmail.com>
2021-12-12 22:00:11 +0800

    Minor cleanup

--- packages/d2l-expo/config/config.ts
+++ packages/d2l-expo/config/config.ts
@@ -3,16 +3,22 @@ import Constants from 'expo-constants';
 const nodeEnv = process.env.NODE_ENV || 'development';
 
 const inProduction = nodeEnv === 'production';
+const inExpo = Constants.manifest && Constants.manifest.debuggerHost;
+const inBrowser = document != null;
 
-//export const apiDomain = 'localhost';
-//export const apiDomain = inProduction ? 'd2l.sg' : '192.168.0.195';
 // In development mode, using Expo Go on mobile, we try to get the developer's LAN IP from the debugger
 // If it's not there, then we assume we are on browser, and get the developer's IP from document.location
+// Source: https://stackoverflow.com/a/49198103/99777
+//
+//const inProduction = manifest.packagerOpts == null;
 export const apiDomain = inProduction
   ? 'd2l.sg'
-  : Constants.manifest && Constants.manifest.debuggerHost
-  ? Constants.manifest.debuggerHost!.split(`:`).shift()
-  : document?.location.hostname;
+  : inExpo
+  ? Constants.manifest!.debuggerHost!.split(`:`).shift()
+  : inBrowser
+  ? document.location.hostname
+  : 'unknown';
+
 console.log('apiDomain:', apiDomain);
 
-export const apiUrl = inProduction ? `//${apiDomain}/v1/graphql` : `http://${apiDomain}:4000/graphql`;
+export const apiUrl = inProduction ? `https://${apiDomain}/v1/graphql` : `http://${apiDomain}:4000/graphql`;
