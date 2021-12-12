ec6b68e4a67fcbea64a567ec05a69ca3725f4f01
Paul Joey Clark <joeytwiddle@gmail.com>
2021-12-12 16:17:24 +0800

    Use a trick to guess the developer's LAN IP

--- packages/d2l-expo/config/config.ts
+++ packages/d2l-expo/config/config.ts
@@ -1,10 +1,18 @@
+import Constants from 'expo-constants';
+
 const nodeEnv = process.env.NODE_ENV || 'development';
 
 const inProduction = nodeEnv === 'production';
 
 //export const apiDomain = 'localhost';
-export const apiDomain = inProduction ? 'd2l.sg' : '192.168.0.195';
+//export const apiDomain = inProduction ? 'd2l.sg' : '192.168.0.195';
+// In development mode, using Expo Go on mobile, we try to get the developer's LAN IP from the debugger
+// If it's not there, then we assume we are on browser, and get the developer's IP from document.location
+export const apiDomain = inProduction
+  ? 'd2l.sg'
+  : Constants.manifest && Constants.manifest.debuggerHost
+  ? Constants.manifest.debuggerHost!.split(`:`).shift()
+  : document?.location.hostname;
+console.log('apiDomain:', apiDomain);
 
-//export const apiUrl = inProduction ? `https://${apiDomain}/graphql` : `http://${apiDomain}:4000/graphql`;
-
-export const apiUrl = inProduction ? `//${apiDomain}/v1/graphql` : `//${apiDomain}:4000/graphql`;
+export const apiUrl = inProduction ? `//${apiDomain}/v1/graphql` : `http://${apiDomain}:4000/graphql`;
