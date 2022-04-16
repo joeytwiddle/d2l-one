9b8a4db7db187ffaeb406d8fe53f0cf8f3621784
Paul Joey Clark <joeytwiddle@gmail.com>
2022-04-16 16:38:26 +0800

    Give upcoming rescues its own ScrollView

--- packages/d2l-expo/screens/DashboardScreen.tsx
+++ packages/d2l-expo/screens/DashboardScreen.tsx
@@ -5,8 +5,13 @@ import EditScreenInfo from '../components/EditScreenInfo';
 import { PaddedBlock } from '../components/Layout';
 import Logo from '../components/Logo';
 import RescueCard from '../components/RescueCard';
-import { LoadingSpinner, Text, View } from '../components/Themed';
-import { useGetAllRescuesQuery, useGetMyRescuesQuery } from '../graphql';
+import { Button, LoadingSpinner, Text, View } from '../components/Themed';
+import {
+  useGetAllRescuesQuery,
+  useGetAvailableRescuesForCurrentUserLazyQuery,
+  useGetAvailableRescuesForCurrentUserQuery,
+  useGetMyRescuesQuery,
+} from '../graphql';
 import useUser from '../hooks/useUser';
 import { RootTabScreenProps } from '../types';
 
@@ -19,43 +24,68 @@ export default function DashboardScreen({ navigation }: RootTabScreenProps<'Dash
   console.log('[Dashboard] user:', user);
   console.log('[Dashboard] myRescues:', myRescues);
 
+  const availableRescuesQuery = useGetAvailableRescuesForCurrentUserQuery();
+  const availableRescues = availableRescuesQuery.data?.availableRescuesForCurrentUser;
+  //
+  // If we want to load the upcoming rescues first, before the larger list of availableRescues, we can use a lazy query
+  // WARNING: Do not use this!  Fir some reason it triggers a refetch of myRescues every 2 seconds.
+  /*
+  const availableRescuesLazyQuery = useGetAvailableRescuesForCurrentUserLazyQuery();
+  const availableRescues = availableRescuesLazyQuery[1].data?.availableRescuesForCurrentUser;
+  React.useEffect(() => {
+    setTimeout(() => {
+      availableRescuesLazyQuery[0]();
+    }, 2000);
+  });
+  */
+
   if (!user) return null;
 
-  // TODO: Introducing PageContainer does not work well here: It makes the cards shrink width to the minimum they can.
   return (
-    <ScrollView>
-      <SafeAreaView>
-        <View style={styles.container}>
-          <Logo />
+    <View style={styles.container}>
+      {/*
+        SafeAreaView doesn't need to wrap the entire content, only the header.  If there is no header, it can just be used for padding
+        https://stackoverflow.com/a/59185120/99777
+      */}
+      <SafeAreaView></SafeAreaView>
+      <Logo />
+      <PaddedBlock>
+        <Text>Welcome {user.name}</Text>
+      </PaddedBlock>
+      {/*<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />*/}
+      <ScrollView style={styles.upcomingRescuesScrollContainer} contentContainerStyle={styles.upcomingRescues}>
+        <Text style={styles.title}>Your upcoming rescues</Text>
+        {!myRescues && <LoadingSpinner />}
+        {myRescues && myRescues.length === 0 && (
           <PaddedBlock>
-            <Text>Welcome {user.name}</Text>
+            <Text>No rescues booked</Text>
           </PaddedBlock>
-          {/*<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />*/}
-          <View style={styles.upcomingRescues}>
-            <Text style={styles.title}>Your upcoming rescues</Text>
-            {!myRescues && <LoadingSpinner />}
-            {myRescues && myRescues.length === 0 && (
-              <PaddedBlock>
-                <Text>No rescues booked</Text>
-              </PaddedBlock>
-            )}
-            {myRescues &&
-              myRescues.map(rescue => (
-                <RescueCard
-                  key={rescue.id}
-                  rescue={rescue}
-                  onPress={() =>
-                    navigation.navigate('BookedRescueScreen', {
-                      rescueId: rescue.id,
-                    })
-                  }
-                />
-              ))}
-          </View>
-          {/*<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />*/}
-        </View>
-      </SafeAreaView>
-    </ScrollView>
+        )}
+        {myRescues &&
+          myRescues.map(rescue => (
+            <RescueCard
+              key={rescue.id}
+              rescue={rescue}
+              onPress={() =>
+                navigation.navigate('BookedRescueScreen', {
+                  rescueId: rescue.id,
+                })
+              }
+            />
+          ))}
+        {/*<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />*/}
+      </ScrollView>
+      {availableRescues && availableRescues.length > 0 ? (
+        <PaddedBlock>
+          <Button
+            title="Start Booking"
+            onPress={() => {
+              navigation.navigate('Rescues');
+            }}
+          ></Button>
+        </PaddedBlock>
+      ) : null}
+    </View>
   );
 }
 
@@ -74,9 +104,12 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
-  upcomingRescues: {
+  upcomingRescuesScrollContainer: {
     width: '100%',
-    flex: 1,
+  },
+  upcomingRescues: {
+    //width: '100%',
+    //flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     paddingVertical: 20,
